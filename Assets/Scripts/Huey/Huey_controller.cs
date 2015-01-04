9d057750ec8eae43ac3d87dfa8b5b86cd27f192e
gozu66 <gozu66@gmail.com>
2015-01-04 22:07:06 +0000

    Character animation test

---
+++ Assets/Scripts/Huey/Huey_controller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Huey_controller : MonoBehaviour 
+{
+	bool isWalking;
+	bool isRunning;
+	bool isIdle;
+
+	public float moveSpeed = 0.0f;
+	public float strafeSpeed = 0.0f;
+
+	CharacterController CharCont;
+
+	Animator anim;
+
+	void Start()
+	{
+		CharCont = GetComponent<CharacterController>();
+		anim = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		anim.SetBool("isWalking", isWalking);
+		anim.SetBool("isRunning", isRunning);
+		anim.SetBool("isIdle", isIdle);
+
+
+		moveSpeed = Input.GetAxis("Vertical") * Time.deltaTime * 10;
+		strafeSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+
+
+		CharCont.Move(transform.forward * moveSpeed);
+//		CharCont.Move(transform.right * strafeSpeed);
+
+		transform.Rotate(Vector3.up, strafeSpeed * 5);
+	}
+}
