5e349d44b45f9e5be890d37c6eb20ded4cf073cf
richie <gozu66@gmail.com>
2015-01-05 00:46:58 +0000

    Animation test, basic walk, run, idle.

--- Assets/Scripts/Huey/Huey_controller.cs
+++ Assets/Scripts/Huey/Huey_controller.cs
@@ -11,29 +11,36 @@ public class Huey_controller : MonoBehaviour
 	public float strafeSpeed = 0.0f;
 
 	CharacterController CharCont;
-
 	Animator anim;
+
+	int xSpeed = 10;
+
+	public float Sensitivity = 1;
 
 	void Start()
 	{
 		CharCont = GetComponent<CharacterController>();
-		anim = GetComponent<Animator>();
+		anim = this.GetComponent<Animator>();
 	}
 
 	void Update()
 	{
+		xSpeed = (isRunning) ? 20 : 10;
+
 		anim.SetBool("isWalking", isWalking);
 		anim.SetBool("isRunning", isRunning);
 		anim.SetBool("isIdle", isIdle);
 
+		moveSpeed = Input.GetAxis("Vertical") * Time.deltaTime * xSpeed;
+		strafeSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed;
 
-		moveSpeed = Input.GetAxis("Vertical") * Time.deltaTime * 10;
-		strafeSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
-
+		transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * Sensitivity);
 
 		CharCont.Move(transform.forward * moveSpeed);
-//		CharCont.Move(transform.right * strafeSpeed);
+		CharCont.Move(transform.right * strafeSpeed);
 
-		transform.Rotate(Vector3.up, strafeSpeed * 5);
+		isWalking = (moveSpeed != 0) ? true : false;
+		isIdle = (moveSpeed == 0) ? true : false;
+		isRunning = (Input.GetKey(KeyCode.LeftShift)) ? true : false;
 	}
 }
