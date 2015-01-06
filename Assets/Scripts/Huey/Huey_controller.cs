b4aaa927a3d5141c70cf8bd2214fe3aaa4c6bdf7
gozu66 <gozu66@gmail.com>
2015-01-06 00:56:18 +0000

    Anim test, added strafe, adjusted animations & animation state machine

--- Assets/Scripts/Huey/Huey_controller.cs
+++ Assets/Scripts/Huey/Huey_controller.cs
@@ -6,6 +6,7 @@ public class Huey_controller : MonoBehaviour
 	bool isWalking;
 	bool isRunning;
 	bool isIdle;
+	bool isStrafing;
 
 	public float moveSpeed = 0.0f;
 	public float strafeSpeed = 0.0f;
@@ -13,7 +14,8 @@ public class Huey_controller : MonoBehaviour
 	CharacterController CharCont;
 	Animator anim;
 
-	int xSpeed = 10;
+	public int xSpeedW = 10;
+	public int xSpeedS = 5;
 
 	public float Sensitivity = 1;
 
@@ -25,14 +27,15 @@ public class Huey_controller : MonoBehaviour
 
 	void Update()
 	{
-		xSpeed = (isRunning) ? 20 : 10;
+		xSpeedW = (isRunning) ? 20 : 10;
 
 		anim.SetBool("isWalking", isWalking);
 		anim.SetBool("isRunning", isRunning);
 		anim.SetBool("isIdle", isIdle);
+		anim.SetBool("isStrafing", isStrafing);
 
-		moveSpeed = Input.GetAxis("Vertical") * Time.deltaTime * xSpeed;
-		strafeSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed;
+		moveSpeed = Input.GetAxis("Vertical") * Time.deltaTime * xSpeedW;
+		strafeSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * xSpeedS;
 
 		transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * Sensitivity);
 
@@ -40,7 +43,11 @@ public class Huey_controller : MonoBehaviour
 		CharCont.Move(transform.right * strafeSpeed);
 
 		isWalking = (moveSpeed != 0) ? true : false;
-		isIdle = (moveSpeed == 0) ? true : false;
+		isStrafing = (strafeSpeed != 0 || Input.GetAxis("Mouse X") != 0) ? true : false;
+		isIdle = (moveSpeed == 0 && strafeSpeed == 0) ? true : false;
+
 		isRunning = (Input.GetKey(KeyCode.LeftShift)) ? true : false;
+
+		anim.speed = 1f;
 	}
 }
